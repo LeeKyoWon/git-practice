27c0fcb49b17508c49f880e6ab7fb5b3d57a9aa6
LeeKyoWon <kskw20@naver.com>
2024-12-10 17:46:01 +0900

    rebase conflict resolved (main, test2)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("one");
-        System.out.println("일");
-        System.out.println("이");
-        System.out.println("삼");
-        System.out.println("4");
+        System.out.println("two");
+        System.out.println("three");
+        System.out.println("four");
         System.out.println("5");
     }
 }
