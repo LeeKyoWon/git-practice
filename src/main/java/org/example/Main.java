03b002a79760c83091293662147e17c2873e6759
LeeKyoWon <kskw20@naver.com>
2024-12-10 17:42:55 +0900

    first test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("일");
         System.out.println("2");
         System.out.println("3");
         System.out.println("4");
