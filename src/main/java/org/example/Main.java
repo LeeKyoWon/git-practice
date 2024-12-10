37adc020bc1e25e40cbcbaff2fd5e8d97e0fc359
LeeKyoWon <kskw20@naver.com>
2024-12-10 17:42:17 +0900

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        System.out.println("commit1");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
     }
 }
