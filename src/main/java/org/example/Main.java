6dcc7f84d1e000881656170ed54c8e9bee3d9b04
LeeKyoWon <kskw20@naver.com>
2024-12-10 17:47:03 +0900

    fourth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
         System.out.println("one");
         System.out.println("일");
-        System.out.println("2");
-        System.out.println("3");
+        System.out.println("이");
+        System.out.println("삼");
         System.out.println("4");
         System.out.println("5");
     }
